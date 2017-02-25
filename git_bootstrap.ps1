6b97b40cbab67524f7100a32a89df128ee16edbc
twainj <jason@2jdevelopment.com>
2017-02-24 20:43:15 -0800

    Create script for bootstrapping Windows dev machines
    This script uses PowerShell to take a clean Windows machine and, using Chocolatey, Boxstarter, and Git, install all of the tools specified in a system_install\system_package.ps1 file which is located in a specified repository

---
+++ git_bootstrap.ps1
@@ -0,0 +1,23 @@
+# This is necessary to execute scripts. If the system complains that it can't run this script, 
+# you might have to run this line by itself first.
+set-executionpolicy remotesigned -scope currentuser -force
+
+echo 'First I need to get a few things from you...'
+$projDirectory = Read-Host -Prompt 'Project Directory (relative to $home)'
+$projDirectory = "$home\$projDirectory"
+$gitRepo = Read-Host -Prompt 'Git Repo'
+$gitUname = Read-Host -Prompt 'Git Username'
+$secGitPass = Read-Host -Prompt 'Git Pass' -AsSecureString
+$gitPass = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($secGitPass))
+echo "Thanks! I'm going to do my thing, check back in a while!"
+
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/mwrock/boxstarter/master/BuildScripts/bootstrapper.ps1'))
+Get-Boxstarter -Force
+Import-Module $env:appdata\boxstarter\boxstarter.chocolatey\boxstarter.chocolatey.psd1
+cinst git -y
+echo "Git credentials are being stored in $home\.gitconfig..."
+Add-Content "$home\.gitconfig" "`n[credential ""$gitRepo""]`n`t$gitUname = $gitPass"
+
+git clone $gitRepo "$projDirectory\Repository"
+Install-BoxstarterPackage -PackageName "$projDirectory\Repository\system_install\system_package.ps1"
